cd11b4c493b5e750b3d8082ed31337a7ba109a72
Jonathan Alvarez <jonathanalvarez@g.harvard.edu>
2022-04-04 12:08:10 -0400

    Major reformatting and comments
    Spend an hour or so reformatting and adding comments to methods and GUI setup. Should be standardized now between RPi and Windows version.

--- windows/CD_GUI_WINDOWS.py
+++ windows/CD_GUI_WINDOWS.py
@@ -11,15 +11,11 @@ import os
 import time
 from dkc_rehamovelib.DKC_rehamovelib import * # Import our library
 from datetime import datetime
+
 # import busio
 # import board
-# import adafruit_mcp3xxx.mcp3008 as MCP
-# from adafruit_mcp3xxx.analog_in import AnalogIn
-
-
 # import adafruit_ads1x15.ads1115 as ADS
 # from adafruit_ads1x15.analog_in import AnalogIn
-
 
 SAMPLES = 1000
 SAMPLES_ = 100
@@ -42,7 +38,6 @@ class Window1(QDialog):
 		self.comports = serial.tools.list_ports.comports(include_links=True)
 		for comport in self.comports:
 			self.portSelect_cb.addItem(comport.description)
-
 		self.startPlotting_pb.clicked.connect(self.connect_to_main) 
 
 	def key_press_event(self, e):
@@ -80,36 +75,40 @@ class Window2(QtWidgets.QMainWindow):
 	def __init__(self, myFES, *args, **kwargs):
 		super(Window2, self).__init__(*args, **kwargs)
 		uic.loadUi('resources/_CD_GUI_Main_Formed.ui', self) # load ui from Qt Designer
-		#connect push buttons and spinners
+		
+		#### Setup GUI Widgets ####
+		#Setup FES Channels and Parameters
 		self.channel_cb.addItems([' ', '1 (Red)', '2 (Blue)', '3 (Black)', '4 (White)'])
-		self.amp_sp.setRange(0, 50)
-		self.dur_sp.setRange(0, 500)
-		self.f_sp.setRange(0, 10)
-		self.np_sp.setRange(0, 100)
+		self.amp_sp.setRange(0, 50) #FES amplitude (mA)
+		self.amp_sp.setValue(15)		
+		self.dur_sp.setRange(0, 500) #FES duration (ms)
+		self.dur_sp.setValue(150)
+		self.f_sp.setRange(0, 10) #FES frequency (Hz)
+		self.f_sp.setValue(1)
+		self.np_sp.setRange(0, 100) #FES number of pulses (int)
+		self.np_sp.setValue(1)
+		
+		#Setup push and check buttons
+		self.commit_pb.clicked.connect(self.update_lcd) #commit parameters to FES system
+		self.target_cb.toggled.connect(self.update_target) #add target line to plot
+		self.download_pb.clicked.connect(self.download) #download data 
+		self.sendPulse_pb.clicked.connect(self.try_fes) #send a single FES burst
+		self.CD_pb.clicked.connect(self.connect_to_Window3) #go to CD window
+
+		#Set start time
+		self.time_start = time.time()
 		self.max_value_reached = 0
 
-		self.time_start = time.time()
-		self.amp_sp.setValue(15)
-		self.dur_sp.setValue(150)
-		self.f_sp.setValue(1)
-		self.np_sp.setValue(1)
-
-		self.commit_pb.clicked.connect(self.update_lcd)
-
-		self.target_cb.toggled.connect(self.update_target)
-		self.download_pb.clicked.connect(self.download)
-		self.sendPulse_pb.clicked.connect(self.update_fes)
-		self.CD_pb.clicked.connect(self.connect_to_Window3)
-
-		#prepare scrolling graph from pyqtgraph
-		self.graphWidget = pg.PlotWidget()
-		self.findChild(QWidget, "TorquePlot").layout().addWidget(self.graphWidget)
-
+		#### Setup PYQTGRAPH ####
+		# Preassign data
 		self.x = list(range(SAMPLES)) 
 		self.y1 = [0] * SAMPLES  
 		self.x_storage = []
 		self.y1_storage = []
 
+		# Create a plot window
+		self.graphWidget = pg.PlotWidget()
+		self.findChild(QWidget, "TorquePlot").layout().addWidget(self.graphWidget)
 		self.graphWidget.setBackground('w')
 		self.graphWidget.setTitle("Central Drive Estimation")
 		self.graphWidget.setLabel('left', 'Torque (ft-lbs)')
@@ -118,17 +117,19 @@ class Window2(QtWidgets.QMainWindow):
 		# self.graphWidget.setYRange(0, 10, padding=1)
 		self.graphWidget.enableAutoRange('y', 0.95)
 		self.graphWidget.addLegend()
+
+		#Prepare scrolling line
 		pen_ref = pg.mkPen(color=(192,192,192), width=1, style=QtCore.Qt.DashLine)
-
 		pen1 = pg.mkPen(color=(0,123,184), width=2)
-
 		self.data_line1 = self.graphWidget.plot(self.x, self.y1, pen=pen1, name="Torque Sensor")
 
+		#### Setup Timer ####
 		self.timer = QtCore.QTimer()
 		self.timer.setInterval(10)
 		self.timer.timeout.connect(self.update_gui)
 		self.timer.start()
 
+		#### Miscellaneous ####
 		# Setup a PAUSE button in a toolbar
 		self.toolbar = self.addToolBar("Pause")
 		self.pause_tb = QAction("Pause", self)
@@ -136,30 +137,29 @@ class Window2(QtWidgets.QMainWindow):
 		self.pause_tb.setCheckable(True)
 		self.toolbar.addAction(self.pause_tb)
 
-	def pause_plotting(self):
-		if self.pause_tb.isChecked():
-			self.timer.stop()
-		else:
-			self.timer.start()
+	#### GUI and PLOTTING METHODS ####
+	def update_gui(self):
+		"""Master method called by timer to update plot."""
+		self.update_plot()
 
 	def update_lcd(self):
+		"""Update the FES parameters to reflect commited values."""
 		self.committedAmp_lbl.display(self.amp_sp.value())
 		self.committedDur_lbl.display(self.dur_sp.value())
 		self.committedF_lbl.display(self.f_sp.value())
-# 		self.committedNp_lbl.display(self.np_sp.value())
+		self.committedNp_lbl.display(self.np_sp.value())
 
 	def update_target(self):
+		"""Add target line in red to plot for participant to try and reach."""
 		if self.target_cb.isChecked():
 			self.target = pg.InfiniteLine(movable=True, angle=0, pen = {'color': 'r', 'width': 10}, label='Torque Target ={value:0.2f}', 
                        labelOpts={'position':0.5, 'color': (200,0,0), 'fill': (200,200,200,50), 'movable': True})
 			self.graphWidget.addItem(self.target)
 		else:
 			self.graphWidget.removeItem(self.target)
-			
-	def update_gui(self):
-		self.update_plot()
 
 	def update_plot(self):
+		"""Update the plot with new data."""
 		self.x = self.add_data(self.x, (time.time()-self.time_start))
 		self.y1 = self.add_data(self.y1, (time.time()%60))
 		
@@ -170,22 +170,52 @@ class Window2(QtWidgets.QMainWindow):
 			# self.max_value_reached = (float(time.time()))
 			# self.committedNp_lbl.display(self.max_value_reached)
 
-
-	def add_data(self, data_buffer, new_data): # append new data to buffer
+	def add_data(self, data_buffer, new_data):
+		"""Store new data in buffer."""
 		data_buffer = data_buffer[1:]
 		data_buffer.append(float(new_data))
 		return data_buffer
 
-	def key_press_event(self, event):
-		if event.key() == Qt.Key_Escape:
-			self.close()
+	#### FES METHODS ####
+	def try_fes(self):
+		"""If FES is enabled, update values and send a pulse."""
+		if self.fesEnable_cb.isChecked():
+			self.update_and_send_pulse_fes()
+		else:
+			pass
 
+	def update_and_send_pulse_fes(self):
+		"""Update FES parameters and send pulse."""
+		amp = int(self.committedAmp_lbl.value()) # [mA]
+		dur = int(self.committedDur_lbl.value()) # [us]
+		f = int(self.committedF_lbl.value()) # hz
+		n_pulse = int(self.committedNp_lbl.value())
+		try:
+			channelNum = int(self.channel_cb.currentText()[0]) # for channels: 1 = red, 2 = blue, 3 = black, 4 = white
+			self.send_pulse_fes(amp, dur, f, n_pulse, channelNum)
+		except:
+			print('No Channel Selected')
+
+	def send_pulse_fes(self, amp, dur, f, n_pulse, channelNum):
+		"""Send pulse to FES."""
+		for i in range(0,n_pulse):
+			if f == 1:
+				myFES.write_pulse(channelNum, [(dur,amp), (50, 0), (dur, -amp)])     # Send pulse every second
+				print('Hasomed Pulse Sent')
+			else:
+				myFES.write_pulse(channelNum, [(dur,amp), (50, 0), (dur, -amp)])     # Send pulse every second
+				print('Hasomed Pulse Sent')
+				time.sleep(1.0/f)		
+
+	#### ADDITIONAL METHODS ####
 	def connect_to_Window3(self):
+		"""Connect to Window3."""
 		self.w3 = Window3(self)
 		self.close()
 		self.w3.show()
 
 	def download(self):
+		"""Download data from GUI."""
 		default_filename = 'CD_' + datetime.now().strftime("%Y%m%d-%H%M%S")
 		filename, _ = QFileDialog.getSaveFileName(self, "Save data file", default_filename, "CSV Files (*.csv)")
 		if filename:
@@ -197,68 +227,51 @@ class Window2(QtWidgets.QMainWindow):
 			msg.setWindowTitle("Success")
 			msg.exec()
 
-	def update_fes(self): #, amp):
-		# self.commitedAmp_lbl.display(self.amp_sp.value())
-		if self.fesEnable_cb.isChecked():
-			self.test_fes()
+	def key_press_event(self, event):
+		"""Handle key press events."""
+		if event.key() == Qt.Key_Escape:
+			self.close()
+
+	def pause_plotting(self):
+		"""Pause plotting by stopping the background timer."""
+		if self.pause_tb.isChecked():
+			self.timer.stop()
 		else:
-			pass
+			self.timer.start()
 
-	def test_fes(self):
-		amp = int(self.committedAmp_lbl.value()) # [mA]
-		dur = int(self.committedDur_lbl.value()) # [us]
-		f = int(self.committedF_lbl.value()) # hz
-		n_pulse = int(self.committedNp_lbl.value())
 
-		try:
-			channelNum = int(self.channel_cb.currentText()[0]) # for channels: 1 = red, 2 = blue, 3 = black, 4 = white
-			self.send_pulse_fes(amp, dur, f, n_pulse, channelNum)
-		except:
-			print('No Channel Selected')
-
-	def send_pulse_fes(self, amp, dur, f, n_pulse, channelNum):
-		for i in range(0,n_pulse):
-			if f == 1:
-				myFES.write_pulse(channelNum, [(dur,amp), (50, 0), (dur, -amp)])     # Send pulse every second
-				print('Hasomed Pulse Sent')
-			else:
-				myFES.write_pulse(channelNum, [(dur,amp), (50, 0), (dur, -amp)])     # Send pulse every second
-				print('Hasomed Pulse Sent')
-				time.sleep(1.0/f)		
-
-	# def closeEvent(self, event):
-	# 	reply = QMessageBox.question(self, 'Window Close', 'Are you sure you want to close the window?',
-	# 			QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-
-	# 	if reply == QMessageBox.Yes:
-	# 		event.accept()
-	# 		self.FES.disconnect()
-	# 	else:
-	# 		event.ignore()
-
+## CENTRAL DRIVE WINDOW:
+# -----------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------
 class Window3(QtWidgets.QMainWindow):
 	def __init__(self, w2, *args, **kwargs):
 		super(Window3, self).__init__(*args, **kwargs)
 		uic.loadUi('resources/_CD_GUI_CDTest_Formed.ui', self) # load ui from Qt Designer
 
+		#### Initialize Variables ####
 		self.w22 = w2
 		self.test_complete = False
+		self.time_start = time.time() 
 
-		self.time_start = time.time()
-
-		#grab values commited to Hasomed Device from Window2
+		#### Setup GUI Widgets ####
+		#Grab values commited to Hasomed Device from Window2
 		self.channel_lbl.setText(self.w22.channel_cb.currentText()) 
-		self.committedAmp_lbl.display(self.w22.committedAmp_lbl.value()) 
-		self.committedDur_lbl.display(self.w22.committedDur_lbl.value())
-		self.committedF_lbl.display(self.w22.committedF_lbl.value())
-		self.committedNp_lbl.display(self.w22.committedNp_lbl.value())
+		self.committedAmp_lbl.display(self.w22.committedAmp_lbl.value()) #FES amplitude (mA)
+		self.committedDur_lbl.display(self.w22.committedDur_lbl.value()) # FES Duration (us)
+		self.committedF_lbl.display(self.w22.committedF_lbl.value()) #FES frequency (Hz)
+		self.committedNp_lbl.display(self.w22.committedNp_lbl.value()) #FES number of pulses (int)
 
-		#connect push buttons and spinners
+		#Connect push buttons and spinners
 		self.target_cb.toggled.connect(self.update_target)
 		self.download_pb.clicked.connect(self.download)
 		self.returnToMain_pb.clicked.connect(self.connect_to_main)
 
-		# 3) Plot in chunks, adding one new plot curve for every 100 samples
+		#### Setup PYQTGRAPH ####
+		# Preassign data
 		self.chunkSize = 100
 		# Remove chunks after we have 10
 		self.maxChunks = 20
@@ -273,10 +286,9 @@ class Window3(QtWidgets.QMainWindow):
 		self.x_storage = []
 		self.y1_storage = []
 
-		#prepare scrolling graph from pyqtgraph
+		# Create a plot window
 		self.graphWidget = pg.PlotWidget()
 		self.findChild(QWidget, "TorquePlot").layout().addWidget(self.graphWidget)
-
 		self.graphWidget.setBackground('w')
 		self.graphWidget.setTitle("Central Drive Estimation")
 		self.graphWidget.setLabel('left', 'Torque (ft-lbs)')
@@ -286,17 +298,18 @@ class Window3(QtWidgets.QMainWindow):
 		self.graphWidget.enableAutoRange('y', 0.95)
 		self.graphWidget.addLegend()		
 
+		#Prepare scrolling line
 		# pen_ref = pg.mkPen(color=(192,192,192), width=1, style=QtCore.Qt.DashLine)
-
 		pen1 = pg.mkPen(color=(0,123,184), width=2)
-
 		self.data_line1 = self.graphWidget.plot(self.x, self.y1, pen=pen1, name="Torque Sensor")
 
+		#### Setup Timer ####
 		self.timer = QtCore.QTimer()
 		self.timer.setInterval(0)
-		self.timer.timeout.connect(self.update_plot)
+		self.timer.timeout.connect(self.update_gui)
 		self.timer.start()
 
+		#### Miscellaneous ####
 		# Setup a PAUSE button in a toolbar
 		self.toolbar = self.addToolBar("Pause")
 		self.pause_tb = QAction("Pause", self)
@@ -304,33 +317,27 @@ class Window3(QtWidgets.QMainWindow):
 		self.pause_tb.setCheckable(True)
 		self.toolbar.addAction(self.pause_tb)
 
-	def pause_plotting(self):
-		if self.pause_tb.isChecked():
-			self.timer.stop()
-		else:
-			self.timer.start()
+
+	#### GUI AND PLOTTING METHODS ####
+	def update_gui(self):
+		"""Master method called by timer to update plot."""
+		self.update_plot()
 
 	def update_lcd(self):
+		"""Update the FES parameters to reflect commited values."""
 		self.amp_lcd.display(self.amp_sp.value())
 
 	def update_target(self):
+		"""Add target line in red to plot for participant to try and reach."""
 		if self.target_cb.isChecked():
 			self.target = pg.InfiniteLine(movable=True, angle=0, pen = {'color': 'r', 'width': 10}, label='Torque Target ={value:0.2f}', 
                        labelOpts={'position':0.5, 'color': (200,0,0), 'fill': (200,200,200,50), 'movable': True})
 			self.graphWidget.addItem(self.target)
 		else:
 			self.graphWidget.removeItem(self.target)
-			
-	def update_vals(self):
-		print(self.y1[-1])
-		if self.y1[-1] < 2:
-			print('here')
-			self.timer.stop()
-		else:
-			print('this is not working')
-			self.timer.start()
-			
+
 	def update_plot(self):
+		"""Update the plot with new data."""
 		# now = pg.ptime.time()
 		# for c in self.curves:
 		# 	c.setPos(-(now-self.startTime), 0)
@@ -374,39 +381,31 @@ class Window3(QtWidgets.QMainWindow):
 		# 	self.sendPulseFES()
 		# self.data_line1.setData(self.x, self.y1)
 
-	def add_data(self, data_buffer, new_data): # append new data to buffer
+	def add_data(self, data_buffer, new_data): 
+		"""Store new data in buffer."""
 		data_buffer = data_buffer[1:]
 		data_buffer.append(float(new_data))
 		return data_buffer
-
-	def key_press_event(self, event):
-		if event.key() == Qt.Key_Escape:
-			self.close()
-
-	def connect_to_main(self):
-		self.w22.show()
-		self.close()
-
-	def download(self):
-		default_filename = 'CD_' + datetime.now().strftime("%Y%m%d-%H%M%S")
-		filename, _ = QFileDialog.getSaveFileName(self, "Save data file", default_filename, "CSV Files (*.csv)")
-		if filename:
-			data_to_save = list(zip(self.x_storage, self.y1_storage))
-			np.savetxt(filename + '.csv', data_to_save, delimiter = ',')
-			msg = QMessageBox()
-			msg.setIcon(QMessageBox.Information)
-			msg.setText("Download completed successfully!")
-			msg.setWindowTitle("Success")
-			msg.exec()
-
-	def update_fes(self): #, amp):
-		# self.commitedAmp_lbl.display(self.amp_sp.value())
+			
+	def update_vals(self):
+		print(self.y1[-1])
+		if self.y1[-1] < 2:
+			print('here')
+			self.timer.stop()
+		else:
+			print('this is not working')
+			self.timer.start()
+			
+	#### FES METHODS ####
+	def try_fes(self): 
+		"""If FES is enabled, update values and send a pulse."""
 		if self.fesEnable_cb.isChecked():
-			self.test_fes()
+			self.update_and_send_pulse_fes()
 		else:
 			pass
 
-	def test_fes(self):
+	def update_and_send_pulse_fes(self):
+		"""Update FES parameters and send pulse."""
 		amp = int(self.committedAmp_lbl.value()) # [mA]
 		dur = int(self.committedDur_lbl.value()) # [us]
 		f = int(self.committedF_lbl.value()) # hz
@@ -419,15 +418,52 @@ class Window3(QtWidgets.QMainWindow):
 			print('No Channel Selected')
 
 	def send_pulse_fes(self, amp, dur, f, n_pulse, channelNum):
+		"""Send pulse to FES."""
 		for i in range(0,n_pulse):
 			if f == 1:
 				myFES.write_pulse(channelNum, [(dur,amp), (50, 0), (dur, -amp)])     # Send pulse every second
 				print('Hasomed Pulse Sent')
 			else:
 				myFES.write_pulse(channelNum, [(dur,amp), (50, 0), (dur, -amp)])     # Send pulse every second
-				print('Hasomedd Pulse Sent')
+				print('Hasomed Pulse Sent')
 				time.sleep(1.0/f)		
+	
+	#### MISCELLANEOUS METHODS ####
+	def connect_to_main(self):
+		"""Connect to main GUI."""
+		self.w22.show()
+		self.close()
 
+	def download(self):
+		"""Download data from GUI."""
+		default_filename = 'CD_' + datetime.now().strftime("%Y%m%d-%H%M%S")
+		filename, _ = QFileDialog.getSaveFileName(self, "Save data file", default_filename, "CSV Files (*.csv)")
+		if filename:
+			data_to_save = list(zip(self.x_storage, self.y1_storage))
+			np.savetxt(filename + '.csv', data_to_save, delimiter = ',')
+			msg = QMessageBox()
+			msg.setIcon(QMessageBox.Information)
+			msg.setText("Download completed successfully!")
+			msg.setWindowTitle("Success")
+			msg.exec()
+
+	def pause_plotting(self):
+		"""Pause plotting by stopping the bacgkround timer."""
+		if self.pause_tb.isChecked():
+			self.timer.stop()
+		else:
+			self.timer.start()
+
+	def key_press_event(self, event):
+		"""Handle key press event."""
+		if event.key() == Qt.Key_Escape:
+			self.close()
+
+
+## SETUP:
+# -----------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------
 app = QtWidgets.QApplication(sys.argv)
 
 #setup FES
