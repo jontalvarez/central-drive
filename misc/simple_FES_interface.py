1c0b12782faea7ce9062082ac67f337daa87b90c
Jonathan Alvarez <jonathanalvarez@g.harvard.edu>
2022-05-23 14:18:09 -0400

    Update simple_FES_interface.py
    Added additional comments for easier use.

--- misc/simple_FES_interface.py
+++ misc/simple_FES_interface.py
@@ -1,15 +1,15 @@
-from dkc_rehamovelib import *          # Import our library
+from dkc_rehamovelib.DKC_rehamovelib import *  # Import our library
 import time
 
 ## CHANGE COM PORT BASED OFF OF DEVICE MANAGER
 COM_PORT = "COM6"
 r = Rehamove_DKC(COM_PORT)            # Open USB port (on Windows)
 
-##MODIFY THESE PARAMETERS##
+##MODIFY THESE PARAMETERS## %test with 10 mA, 500 us, 1 Hz, 2 pulses on forearm to check if it works
 amp = 150 #mA
-dur = 500 #us
+dur = 550 #us
 f = 100 #Hz
-n_pulse = 10
+n_pulse = 15 #15 normally
 channelNum = 2 # for channels: 1 = red, 2 = blue, 3 = black, 4 = white
 ####
 
@@ -17,16 +17,19 @@ r.connect()
 r.initialize()
 
 for i in range(0,n_pulse):
-   # r.write_pulse(channelNum, [(dur,amp), (50, 0), (dur, -amp)])     # Send pulse every second
-   # time.sleep(1.0/f)
-
-   # #Monophasic
-   # r.write_pulse(channelNum, [(dur, amp)])     # Send pulse every second
-   # time.sleep(1.0/f - dur)
-
+   print(dur)
    #Biphasic
-   r.write_pulse(channelNum, [(int((dur-50)/2),amp), (50, 0), (int((dur-50)/2), -amp)])     # Send pulse every second
+   r.write_pulse(channelNum, [(int((dur)/2),amp), (int((dur)/2), -amp)])     # Send pulse every second
    time.sleep(1.0/f - (dur*(10**-6)))
+
+# while dur <= 600:
+#    for i in range(0,n_pulse):
+#       print(dur)
+#       #Biphasic
+#       r.write_pulse(channelNum, [(int((dur)/2),amp), (int((dur)/2), -amp)])     # Send pulse every second
+#       # time.sleep(1.0/f - (dur*(10**-6)))
+#       time.sleep(3)
+#    dur = dur + 50
 
 time.sleep(1)
 r.close()
